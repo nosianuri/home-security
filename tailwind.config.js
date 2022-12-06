4dd8e8f8e75b859ab5e38a96972ed9910ea084d3
nosianuri <nurinosia@gmail.com>
2022-12-07 04:09:58 +0600

    solve error

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          primary: "##1e73be",
+          primary: "#1e73be",
           secondary: "#4d7c0f",
           accent: "#37cdbe",
           neutral: "#3d4451",
