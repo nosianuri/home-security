17b661a5d0d0dd366bbd9d23341431a38b461599
nosianuri <nurinosia@gmail.com>
2022-12-10 02:41:25 +0600

    Update topheader font style

--- src/components/TopHeader.js
+++ src/components/TopHeader.js
@@ -12,7 +12,7 @@ const TopHeader = () => {
                 <h3 className='text-lg' >*Call to get pro Install + $0 Activation</h3>
                 <p className='text-sm'>*New customers only.</p>
             </div>
-            <div className='btn relative call-btn bg-[#7743DB] lg:mr-5 text-white font-bold mt-3'>
+            <div className='btn relative call-btn bg-[#7743DB] lg:mr-5 text-white  mt-3'>
             <div className='h-[20px] w-[19px] absolute bg-[#7743DB] -top-[10px] right-[70px] rotate-45'></div>
             Certified live specialist!
             </div>
