e543d0e7a54304adac99c41cb9cf2a5e5faa316b
nosianuri <nurinosia@gmail.com>
2022-12-13 23:56:46 +0600

    Added unbounded

--- src/components/TopBanner.js
+++ src/components/TopBanner.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import './TopBanner.css';
 
 const TopBanner = () => {
   return (
-    <div className='bg-white text-center lg:m-16 m-8 lg:pl-32 lg:pr-32 lg:text-4xl text-xl !leading-normal'>Looking for a unique and customized Smart Security system for your Beautiful Home or Corporate Office?</div>
+    <div className='bg-white text-center lg:m-16 m-8 lg:pl-32 lg:pr-32 lg:text-4xl text-xl topbanner-text !leading-normal'>Looking for a unique and customized Smart Security system for your Beautiful Home or Corporate Office?</div>
   )
 }
 
